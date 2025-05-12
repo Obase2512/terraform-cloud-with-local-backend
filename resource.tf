6e31299be2e503278348a03587e9fcf3028c5b46
Obase2512 <112844326+Obase2512@users.noreply.github.com>
2025-05-12 16:31:25 -0500

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "thalia" {
   ami = data.aws_ami.guress.id
   instance_type = var.instance_type
   key_name = var.key_pair  #keypair is region specific
-  count = 2
+  count = 3
 
   tags = {
     "Name" = "Aliana-${count.index}"
