3edd12b85aedbc2da28ae08b732e1db337a8bcbd
Obase2512 <112844326+Obase2512@users.noreply.github.com>
2025-05-11 23:25:35 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -13,5 +13,7 @@ terraform {
 #provider block
 provider "aws" {
   region  = var.aws_region
+  access_key =var.aws_secret_key
+  secret_key =var.aws_secret_key
   profile = "Guru"     #view profile #cat ~/.aws/credentials OR edit profile #vi ~/.aws/credentials
 }
